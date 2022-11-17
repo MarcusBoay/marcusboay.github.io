70ae5ad1c20207374a0c16f72e4ba7787cd196fa
Marcus Boay <marcusboay@hotmail.com>
2022-11-16 22:03:33 -0500

    Updates

--- precache-manifest.fda543df519af2d1e81b132662ba07bf.js
+++ precache-manifest.fda543df519af2d1e81b132662ba07bf.js
@@ -1,27 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c90223529ca460b5fdd1e50302e1a60b",
+    "revision": "0bba9a54a04b5e63e2769de087cd6c65",
     "url": "/index.html"
   },
   {
-    "revision": "052c4f25e06ac8524a3c",
-    "url": "/static/css/main.c28e0367.chunk.css"
+    "revision": "60bfc2afd3d75039d20a",
+    "url": "/static/css/main.4baa5e75.chunk.css"
   },
   {
-    "revision": "696bfed7c6df5a0014f0",
-    "url": "/static/js/2.1a6f868d.chunk.js"
+    "revision": "27869c402b7ad89e3e15",
+    "url": "/static/js/2.07e1012a.chunk.js"
   },
   {
-    "revision": "052c4f25e06ac8524a3c",
-    "url": "/static/js/main.203722e7.chunk.js"
+    "revision": "60bfc2afd3d75039d20a",
+    "url": "/static/js/main.93208d38.chunk.js"
   },
   {
     "revision": "1c2cd64ed15d48a260cf",
     "url": "/static/js/runtime-main.48101db0.js"
-  },
-  {
-    "revision": "aafc086022762fa6c55a892510ab4b47",
-    "url": "/static/media/hacked.aafc0860.jpg"
   },
   {
     "revision": "7e15044826fc927b97cdd8b440bf9be1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f4aeaf859b4e36020694a888c57b0ae.js"
+  "/precache-manifest.fda543df519af2d1e81b132662ba07bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
